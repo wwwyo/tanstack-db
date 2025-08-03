ccc7eaab02cfbe75278abd1c75465c8aa6a99a5a
yuito.watanabe <w.ytmrm11811@gmail.com>
2025-08-03 16:56:02 +0900

    ini

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { createCollection, eq, useLiveQuery } from '@tanstack/react-db'
+import { queryCollectionOptions } from '@tanstack/query-db-collection'
+
+function App() {
+  return (
+    <div>
+      <h1>TanStack DB Quick Start</h1>
+      <p>Environment is ready for TanStack DB development!</p>
+    </div>
+  )
+}
+
+export default App
